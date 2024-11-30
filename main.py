a4c0de6a78b4b2427e0cc16e30bf4f004ccce121
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-11-30 18:22:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import sys
 from config import API_ID, API_HASH, TOKENS
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
