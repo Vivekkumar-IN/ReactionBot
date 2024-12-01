148a830eab69bb1e3a2de3766c1ee9dd2c8b2d28
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-12-01 09:20:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from telethon.tl.types import PeerChannel
 from logging.handlers import RotatingFileHandler
 from config import API_ID, API_HASH, TOKENS
 
+from .bot import app
+
+
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
@@ -20,41 +23,6 @@ logging.basicConfig(
 )
 
 log = logging.getLogger("Bot")
-
-
-class You:
-    def __init__(self):
-        self.api_id = API_ID
-        self.api_hash = API_HASH
-        self.tokens = TOKENS
-        self.clients = [
-            TelegramClient(f"bot_{token[:10]}", self.api_id, self.api_hash)
-            for token in self.tokens
-        ]
-
-    async def start(self):
-        for client, token in zip(self.clients, self.tokens):
-            await client.start(bot_token=token)
-            log.info(f"Bot {(await client.get_me()).username} started successfully.")
-        log.info("All bots started successfully.")
-
-        tasks = [client.run_until_disconnected() for client in self.clients]
-        await asyncio.gather(*tasks)
-
-    async def disconnect(self):
-        log.info("Stopping all bots...")
-        await asyncio.gather(*(client.disconnect() for client in self.clients))
-        log.info("All bots stopped.")
-
-    def on(self, event: events.common.EventBuilder):
-        def decorator(f):
-            for client in self.clients:
-                client.add_event_handler(f, event)
-            return f
-        return decorator
-
-
-app = You()
 
 
 @app.on(events.NewMessage(pattern="^/start", func=lambda e: e.is_private))
