88152cede58077813eea7b1dd0fe0f8079dd14e6
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-12-01 12:22:17 +0530

    Update reaction.py

--- plugins/Reaction/reaction.py
+++ plugins/Reaction/reaction.py
@@ -9,9 +9,8 @@ import logging
 
 reactions = ["👍", "👎", "❤", "🔥", "🥰", "👏", "😁", "🤔", "🤯", "😱", "🤬", "😢", "🎉", "🤩", "🤮", "💩", "🙏", "👌", "🕊", "🤡", "🥱", "🥴", "😍", "🐳", "❤‍🔥", "🌚", "🌭", "💯", "🤣", "⚡", "🍌", "🏆", "💔", "🤨", "😐", "🍓", "🍾", "💋", "🖕", "😈", "😴", "😭", "🤓", "👻", "👨‍💻", "👀", "🎃", "🙈", "😇", "😨", "🤝", "✍", "🤗", "🫡", "🎅", "🎄", "☃", "💅", "🤪", "🗿", "🆒", "💘", "🙉", "🦄", "😘", "💊", "🙊", "😎", "👾", "🤷‍♂", "🤷", "🤷‍♀", "😡"]
 
-@app.on(events.NewMessage())
+@app.on(events.NewMessage(func = lambda e: isinstace (e.message.peer_id, PeerChannel))
 async def react(event):
-    logging.info(event.stringify())
     reaction = [ReactionEmoji(emoticon=choice(reactions))]
     await event.client(SendReactionRequest(
     peer=await event.get_chat(),
