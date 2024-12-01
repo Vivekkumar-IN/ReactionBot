d23de4f61e47959178c0207863e53d0ff93472ee
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-12-01 13:39:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,15 +13,22 @@ class You:
             TelegramClient(f"bot_{token[:10]}", self.api_id, self.api_hash)
             for token in self.tokens
         ]
+        self.handlers = []
 
     async def start(self):
         for client, token in zip(self.clients, self.tokens):
             await client.start(bot_token=token)
+            await self._add_available_handlers(client)
+            
             print(f"Bot {(await client.get_me()).username} started successfully.")
         print("All bots started successfully.")
 
         tasks = [client.run_until_disconnected() for client in self.clients]
         await asyncio.gather(*tasks)
+
+    async def _add_available_handlers(client):
+        for func, event in self.handlers:
+            client.add_event_handler(func, event)
 
     async def disconnect(self):
         print("Stopping all bots...")
@@ -30,8 +37,7 @@ class You:
 
     def on(self, event: events.common.EventBuilder):
         def decorator(f):
-            for client in self.clients:
-                client.add_event_handler(f, event)
+            self.handlers.append((f, event))
             return f
         return decorator
 
